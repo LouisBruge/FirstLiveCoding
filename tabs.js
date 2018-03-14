567ee33fb1461ef64124943802e3e750091d092f
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-03-14 09:25:59 +0100

    tabs js file

---
+++ tabs.js
@@ -0,0 +1,12 @@
+console.log('==== LES TABLEAUX ====')
+
+let deadpool = 'Deadpool'
+let movies = [deadpool, 'X-Men', 'Captain America']
+
+console.log(movies[2])
+
+console.log(movies.length)
+
+console.log(movies)
+
+console.log('==== === ======== ====')
