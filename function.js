638c04ecd52987eb15c516cac7af89266ad26014
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-03-14 09:25:45 +0100

    function js file

---
+++ function.js
@@ -0,0 +1,14 @@
+console.log('==== LES FONCTIONS ====')
+
+function hello (name) {
+  console.log(`Salut ${name}.`)
+}
+
+let salut = (name) => {
+  console.log('Bonjour ' + name + '.')
+}
+
+hello('Julien')
+salut('Louis')
+
+console.log('==== === ========= ====')
